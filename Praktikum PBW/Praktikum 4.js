29b1c72fe7a52f2d7daa46d2630df39e915ea231
erikasd1 <erksd123@gmail.com>
2025-03-26 11:07:14 +0700

    Praktikum 4 JavaScript

---
+++ Praktikum PBW/Praktikum 4.js
@@ -0,0 +1,78 @@
+// // alert('hai') //POPUP
+// // console.log('ini sebuah console') //INSPECT
+
+// // var usia = 19
+// // usia = 20
+// // const x =1
+// // let y = 2
+// // console.log('umur saya ' + usia)
+
+// // function contohLet(){
+// //     let y = 10;
+// //     if (true){
+// //         let y = 20;
+// //         console.log("Nilai dalam blok = ", y);
+// //     }
+// //     console.log("Nilai luar blok = ", y)
+// // }
+// // contohLet();
+
+// let nama = 'Erika Sita Dewi' //STRING
+// let usia = 19 //INTEGER
+// let tinggiBadan = 167.5 //DOUBLE/FLOAT
+// let beratBadan
+// beratBadan = 60.2
+// let uang = 3
+
+// // if (uang == null){
+// //     uang = "belum ada"
+// // }else{
+// //     uang = "udah ada"
+// // }
+
+// switch (uang){
+//     case 1: 
+//         uang = "punya tapi dikit"
+//         break;
+
+//     case 2:
+//         uang = "punya cukup"
+//         break;
+
+//     case 3:
+//         uang = "tidak ada"
+//         break;
+// }
+
+// let uangAwal = 20000
+// let uangTambahan = 17000
+// let uangTotal = uangAwal + uangTambahan //algoritma
+
+// // alert('nama saya adalah ' + nama);
+// alert(
+//     `nama saya ${nama}, usia saya ${usia} tahun, tinggi badan saya adalah ${tinggiBadan}cm, berat badan saya ${beratBadan}kg, dan saya ${uang} uang`
+// )
+// alert(`uang awal saya sebesar Rp.${uangAwal}, dan saya diberikan uang oleh orang tua sebesar Rp.${uangTambahan}, jadi uang saya sekarang sebesar Rp.${uangTotal}`)
+
+// let namaPanggilan = [
+//     'Erik', //INDEX 0
+//     'Erika', //INDEX 1
+//     'Sita' //INDEX 2
+// ]
+
+// let mahasiswa = {
+//     nama : "Rina",
+//     usia : 20,
+//     jurusan : "Informatika"
+// }; //OBJECT
+
+// console.log(mahasiswa.jurusan)
+
+
+// namaPanggilan.push('Erick', 'Dewi')  //PUSH:MENAMBAHKAN ISI ARRAY DI LUAR
+// namaPanggilan.shift()//SHIFT: MENGHAPUS SALAH SATU INDEX ARRAY DI AWAL
+// namaPanggilan.pop()//SHIFT: MENGHAPUS SALAH SATU INDEX ARRAY DI AKHIR
+// alert(namaPanggilan)
+
+// console.log(namaPanggilan.length) //MENAMPILKAN PANJANG ARRAY
+
