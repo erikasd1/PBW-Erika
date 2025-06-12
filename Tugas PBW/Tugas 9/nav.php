4f0e07b3965545ff4d50161e7ef81bfb23d6d29e
erikasd1 <erksd123@gmail.com>
2025-06-12 13:34:23 +0700

    Tugas 9

---
+++ Tugas PBW/Tugas 9/nav.php
@@ -0,0 +1,30 @@
+<!-- Menu Navigasi -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="/TugasPBW/Tugas9/index.php">Toko Buku Online Erika</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarNav" aria-controls="navbarNav"
+            aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/TugasPBW/Tugas9/index.php">Daftar Buku</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/TugasPBW/Tugas9/buku/tambah_buku.php">Tambah Buku</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/TugasPBW/Tugas9/transaksi/transaksi.php">Buat Pesanan</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/TugasPBW/Tugas9/transaksi/lihat_transaksi.php">Lihat Pesanan</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/TugasPBW/Tugas9/buku/hapus.php">Hapus Buku</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
